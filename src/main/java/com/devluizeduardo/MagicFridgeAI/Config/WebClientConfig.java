c390a4b5807ce0e9b7b3d7645610778d36929a45
Luiz Eduardo <devluizeduardo@gmail.com>
2026-05-26 22:09:33 -0300

    feat:add WebClient configuration

---
+++ src/main/java/com/devluizeduardo/MagicFridgeAI/Config/WebClientConfig.java
@@ -0,0 +1,21 @@
+package com.devluizeduardo.MagicFridgeAI.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder()
+                .baseUrl(geminiApiUrl)
+                .build();
+
+    }
+}
