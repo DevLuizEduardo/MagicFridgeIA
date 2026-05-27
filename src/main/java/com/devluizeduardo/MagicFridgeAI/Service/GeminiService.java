3c61779e1d3c3ad7f3a2438cc71357ba029472b0
Luiz Eduardo <devluizeduardo@gmail.com>
2026-05-26 22:15:48 -0300

    add metodo do meu Service que consome a API externa Gemini

---
+++ src/main/java/com/devluizeduardo/MagicFridgeAI/Service/GeminiService.java
@@ -0,0 +1,57 @@
+package com.devluizeduardo.MagicFridgeAI.Service;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class GeminiService {
+
+    private final WebClient webClient;
+    private String apiKey = System.getenv("API_KEY");
+
+    public GeminiService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+
+
+public Mono<String> generateRecipe(){
+        String prompt="Me sugira uma receita simples com ingredientes comuns" +
+                "Reponda em formato Json ";
+
+    Map<String,Object>requestBody = Map.of(
+            "contents",List.of(Map.of("parts",
+                    List.of(Map.of("text",prompt)))));
+    return webClient.post()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .header("x-goog-api-key",apiKey)
+            .bodyValue(requestBody)
+            .retrieve()
+            .bodyToMono(Map.class)
+            .map(response -> {
+                var candidates = (List<Map<String, Object>>) response.get("candidates");
+
+
+                if (candidates != null && !candidates.isEmpty()) {
+                    var candidate = candidates.get(0);
+                    var content = (Map<String,Object>)candidate.get("content");
+                if(content!=null&& !content.isEmpty()){
+                    var parts = (List<Map<String, Object>>) content.get("parts");
+
+                    if (parts != null && !parts.isEmpty()) {
+                        return parts.get(0).get("text").toString();
+                    }
+                }}
+                return "Nenhuma receita foi gerada";
+            });
+
+
+
+    }
+}
