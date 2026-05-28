1f2ae928e3f118bb31a9d101630088b20da57e5e
Luiz Eduardo <devluizeduardo@gmail.com>
2026-05-28 13:14:06 -0300

    feat:Adiciona a dependencia FoodItemService e passa o .listarItens() no meu metodo @GET

--- src/main/java/com/devluizeduardo/MagicFridgeAI/Controller/RecipeController.java
+++ src/main/java/com/devluizeduardo/MagicFridgeAI/Controller/RecipeController.java
@@ -1,24 +1,30 @@
 package com.devluizeduardo.MagicFridgeAI.Controller;
 
+import com.devluizeduardo.MagicFridgeAI.Model.FoodItemDTO;
+import com.devluizeduardo.MagicFridgeAI.Service.FoodItemService;
 import com.devluizeduardo.MagicFridgeAI.Service.GeminiService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @RestController
 public class RecipeController {
 
-    private GeminiService geminiService;
+    private final GeminiService geminiService;
+    private final FoodItemService foodItemService;
 
-    public RecipeController(GeminiService geminiService) {
+    public RecipeController(GeminiService geminiService, FoodItemService foodItemService) {
         this.geminiService = geminiService;
+        this.foodItemService = foodItemService;
     }
-
 
     @GetMapping("/generate")
     public Mono<ResponseEntity<String>> generatRecipe(){
-     return geminiService.generateRecipe()
+        List<FoodItemDTO> foodItems = foodItemService.listarItens();
+     return geminiService.generateRecipe(foodItems)
              .map(recipe->ResponseEntity.ok(recipe))
              .defaultIfEmpty(ResponseEntity.noContent().build());
     }
